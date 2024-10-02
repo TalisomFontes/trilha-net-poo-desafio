d9c6cf1c953de27a51e369d70779aa84eaeb38c2
Talisom Fontes <talisomffa@gmail.com>
2024-10-02 11:06:12 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testado!!
+
+Nokia nokia = new Nokia("84325968", "Nokia 3310", "11122222", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("", "Iphone 11", "654321", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
